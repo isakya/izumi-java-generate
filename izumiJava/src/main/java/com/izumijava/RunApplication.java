09e6d0e5da354ed46eecef370c16740876942981
isakya <1016361268@qq.com>
2023-05-22 18:59:23 +0800

    读取表

---
+++ izumiJava/src/main/java/com/izumijava/RunApplication.java
@@ -0,0 +1,10 @@
+package com.izumijava;
+
+import com.izumijava.builder.BuildTable;
+
+public class RunApplication {
+    public static void main(String[] args) {
+        BuildTable.getTables();
+
+    }
+}
